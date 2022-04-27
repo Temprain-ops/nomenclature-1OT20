5884e476f266b7814391ae0394b50490830a4fc2
Valynets Ilya <67782820+Temprain-ops@users.noreply.github.com>
2022-04-27 13:30:48 +0300

    Updated Test part

--- ui/ui_TestConfiguration.py
+++ ui/ui_TestConfiguration.py
@@ -50,15 +50,16 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.radioButton_2 = QRadioButton(self.centralwidget)
-        self.radioButton_2.setObjectName(u"radioButton_2")
+        self.SelfRadioButton = QRadioButton(self.centralwidget)
+        self.SelfRadioButton.setObjectName(u"SelfRadioButton")
+        self.SelfRadioButton.setChecked(True)
 
-        self.horizontalLayout_3.addWidget(self.radioButton_2)
+        self.horizontalLayout_3.addWidget(self.SelfRadioButton)
 
-        self.radioButton = QRadioButton(self.centralwidget)
-        self.radioButton.setObjectName(u"radioButton")
+        self.ControlRadioButton = QRadioButton(self.centralwidget)
+        self.ControlRadioButton.setObjectName(u"ControlRadioButton")
 
-        self.horizontalLayout_3.addWidget(self.radioButton)
+        self.horizontalLayout_3.addWidget(self.ControlRadioButton)
 
 
         self.verticalLayout.addLayout(self.horizontalLayout_3)
@@ -69,45 +70,40 @@ class Ui_MainWindow(object):
 
         self.gridLayout_3 = QGridLayout()
         self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.checkBox_4 = QCheckBox(self.centralwidget)
-        self.checkBox_4.setObjectName(u"checkBox_4")
+        self.HundredcheckBox = QCheckBox(self.centralwidget)
+        self.HundredcheckBox.setObjectName(u"HundredcheckBox")
 
-        self.gridLayout_3.addWidget(self.checkBox_4, 1, 1, 1, 1)
+        self.gridLayout_3.addWidget(self.HundredcheckBox, 1, 1, 1, 1)
 
-        self.checkBox_2 = QCheckBox(self.centralwidget)
-        self.checkBox_2.setObjectName(u"checkBox_2")
+        self.QuarterCheckBox = QCheckBox(self.centralwidget)
+        self.QuarterCheckBox.setObjectName(u"QuarterCheckBox")
 
-        self.gridLayout_3.addWidget(self.checkBox_2, 0, 1, 1, 1)
+        self.gridLayout_3.addWidget(self.QuarterCheckBox, 0, 1, 1, 1)
 
-        self.checkBox_6 = QCheckBox(self.centralwidget)
-        self.checkBox_6.setObjectName(u"checkBox_6")
+        self.TenCheckBox = QCheckBox(self.centralwidget)
+        self.TenCheckBox.setObjectName(u"TenCheckBox")
 
-        self.gridLayout_3.addWidget(self.checkBox_6, 2, 1, 1, 1)
+        self.gridLayout_3.addWidget(self.TenCheckBox, 0, 0, 1, 1)
 
-        self.checkBox = QCheckBox(self.centralwidget)
-        self.checkBox.setObjectName(u"checkBox")
+        self.TwoHundredCheckBox = QCheckBox(self.centralwidget)
+        self.TwoHundredCheckBox.setObjectName(u"TwoHundredCheckBox")
 
-        self.gridLayout_3.addWidget(self.checkBox, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.TwoHundredCheckBox, 2, 0, 1, 1)
 
-        self.checkBox_5 = QCheckBox(self.centralwidget)
-        self.checkBox_5.setObjectName(u"checkBox_5")
+        self.HalfHundredCheckBox = QCheckBox(self.centralwidget)
+        self.HalfHundredCheckBox.setObjectName(u"HalfHundredCheckBox")
 
-        self.gridLayout_3.addWidget(self.checkBox_5, 2, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.HalfHundredCheckBox, 1, 0, 1, 1)
 
-        self.checkBox_8 = QCheckBox(self.centralwidget)
-        self.checkBox_8.setObjectName(u"checkBox_8")
+        self.HalfMillionCheckBox = QCheckBox(self.centralwidget)
+        self.HalfMillionCheckBox.setObjectName(u"HalfMillionCheckBox")
 
-        self.gridLayout_3.addWidget(self.checkBox_8, 3, 1, 1, 1)
+        self.gridLayout_3.addWidget(self.HalfMillionCheckBox, 2, 1, 1, 1)
 
-        self.checkBox_7 = QCheckBox(self.centralwidget)
-        self.checkBox_7.setObjectName(u"checkBox_7")
+        self.MillionCheckBox = QCheckBox(self.centralwidget)
+        self.MillionCheckBox.setObjectName(u"MillionCheckBox")
 
-        self.gridLayout_3.addWidget(self.checkBox_7, 3, 0, 1, 1)
-
-        self.checkBox_3 = QCheckBox(self.centralwidget)
-        self.checkBox_3.setObjectName(u"checkBox_3")
-
-        self.gridLayout_3.addWidget(self.checkBox_3, 1, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.MillionCheckBox, 3, 0, 1, 1)
 
 
         self.verticalLayout.addLayout(self.gridLayout_3)
@@ -122,19 +118,19 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_4.addItem(self.horizontalSpacer_2)
 
-        self.Back = QPushButton(self.centralwidget)
-        self.Back.setObjectName(u"Back")
+        self.BackButton = QPushButton(self.centralwidget)
+        self.BackButton.setObjectName(u"BackButton")
 
-        self.horizontalLayout_4.addWidget(self.Back)
+        self.horizontalLayout_4.addWidget(self.BackButton)
 
         self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         self.horizontalLayout_4.addItem(self.horizontalSpacer)
 
-        self.Begin = QPushButton(self.centralwidget)
-        self.Begin.setObjectName(u"Begin")
+        self.BeginButton = QPushButton(self.centralwidget)
+        self.BeginButton.setObjectName(u"BeginButton")
 
-        self.horizontalLayout_4.addWidget(self.Begin)
+        self.horizontalLayout_4.addWidget(self.BeginButton)
 
         self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
@@ -178,18 +174,17 @@ class Ui_MainWindow(object):
         self.action_2.setText(QCoreApplication.translate("MainWindow", u"\u0417\u0430\u0432\u0435\u0440\u0448\u0438\u0442\u044c \u0442\u0435\u0441\u0442", None))
         self.action_4.setText(QCoreApplication.translate("MainWindow", u"\u0412\u044b\u0445\u043e\u0434", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"\u041d\u0430\u0441\u0442\u0440\u043e\u0439\u043a\u0430 \u0442\u0435\u0441\u0442\u0438\u0440\u043e\u0432\u0430\u043d\u0438\u044f", None))
-        self.radioButton_2.setText(QCoreApplication.translate("MainWindow", u"\u0421\u0430\u043c\u043e\u043a\u043e\u043d\u0442\u0440\u043e\u043b\u044c", None))
-        self.radioButton.setText(QCoreApplication.translate("MainWindow", u"\u041a\u043e\u043d\u0442\u0440\u043e\u043b\u044c \u0417\u043d\u0430\u043d\u0438\u0439", None))
-        self.checkBox_4.setText(QCoreApplication.translate("MainWindow", u"1:100000", None))
-        self.checkBox_2.setText(QCoreApplication.translate("MainWindow", u"1:25000", None))
-        self.checkBox_6.setText(QCoreApplication.translate("MainWindow", u"1:300000", None))
-        self.checkBox.setText(QCoreApplication.translate("MainWindow", u"1:10000", None))
-        self.checkBox_5.setText(QCoreApplication.translate("MainWindow", u"1:200000", None))
-        self.checkBox_8.setText(QCoreApplication.translate("MainWindow", u"1:1000000", None))
-        self.checkBox_7.setText(QCoreApplication.translate("MainWindow", u"1:500000", None))
-        self.checkBox_3.setText(QCoreApplication.translate("MainWindow", u"1:50000", None))
-        self.Back.setText(QCoreApplication.translate("MainWindow", u"\u041d\u0430\u0437\u0430\u0434", None))
-        self.Begin.setText(QCoreApplication.translate("MainWindow", u"\u041d\u0430\u0447\u0430\u0442\u044c \u0442\u0435\u0441\u0442", None))
+        self.SelfRadioButton.setText(QCoreApplication.translate("MainWindow", u"\u0421\u0430\u043c\u043e\u043a\u043e\u043d\u0442\u0440\u043e\u043b\u044c", None))
+        self.ControlRadioButton.setText(QCoreApplication.translate("MainWindow", u"\u041a\u043e\u043d\u0442\u0440\u043e\u043b\u044c \u0417\u043d\u0430\u043d\u0438\u0439", None))
+        self.HundredcheckBox.setText(QCoreApplication.translate("MainWindow", u"1:100000", None))
+        self.QuarterCheckBox.setText(QCoreApplication.translate("MainWindow", u"1:25000", None))
+        self.TenCheckBox.setText(QCoreApplication.translate("MainWindow", u"1:10000", None))
+        self.TwoHundredCheckBox.setText(QCoreApplication.translate("MainWindow", u"1:200000", None))
+        self.HalfHundredCheckBox.setText(QCoreApplication.translate("MainWindow", u"1:50000", None))
+        self.HalfMillionCheckBox.setText(QCoreApplication.translate("MainWindow", u"1:500000", None))
+        self.MillionCheckBox.setText(QCoreApplication.translate("MainWindow", u"1:1000000", None))
+        self.BackButton.setText(QCoreApplication.translate("MainWindow", u"\u041d\u0430\u0437\u0430\u0434", None))
+        self.BeginButton.setText(QCoreApplication.translate("MainWindow", u"\u041d\u0430\u0447\u0430\u0442\u044c \u0442\u0435\u0441\u0442", None))
         self.menu.setTitle(QCoreApplication.translate("MainWindow", u"\u0424\u0430\u0439\u043b", None))
     # retranslateUi
 
--- widget.py
+++ widget.py
@@ -1,20 +1,121 @@
 # This Python file uses the following encoding: utf-8
 import os
 import sys
-
-from PySide2.QtWidgets import QApplication, QWidget, QMainWindow
+from PySide2 import QtGui
+from PySide2.QtWidgets import QApplication, QWidget, QMainWindow, QAbstractItemDelegate, QTableWidgetItem
 from PySide2.QtCore import QFile, QIODevice
 from PySide2.QtUiTools import QUiLoader
 import ui.ui_mainwindow
 import ui.ui_TestConfiguration
 import ui.ui_TestProgress
+import gerenerate_nomenclature.generator as gen
+from nomenclatures import get_nomenclatures
+from PyQt5 import QtCore, QtWidgets
 
 
-class TestProgress(QMainWindow):
-    def __init__(self):
-        super(TestProgress, self).__init__()
+class TestProgressWindow(QMainWindow):
+    def __init__(self, TestConfigurationWindow, MainWindow):
+        super(TestProgressWindow, self).__init__()
         self.ui = ui.ui_TestProgress.Ui_MainWindow()
         self.ui.setupUi(self)
+        self.initialize_links(MainWindow, TestConfigurationWindow)
+        self.initialize_table()
+        self.set_signals()
+
+    def initialize_links(self, MainWindow, TestConfigurationWindow):
+        self.mw = MainWindow
+        self.tcw = TestConfigurationWindow
+        print("TPW init")
+
+    def initialize_table(self):
+        for i in range(3):
+            for j in range(3):
+                self.ui.MapTable.item(i, j).setTextAlignment(QtCore.Qt.AlignCenter)
+#        self.ui.MapTable.item(0, 0).setBackground(QtGui.QBrush(QtGui.QColor(128, 0, 0)))
+
+    def clear_table(self):
+        for i in range(3):
+            for j in range(3):
+                self.ui.MapTable.setItem(i, j, QTableWidgetItem(""))
+
+    def set_signals(self):
+        self.ui.BackButton.clicked.connect(self.backButtonClicked)
+        self.ui.NextButton.clicked.connect(self.nextButtonClicked)
+
+    def toggle_test_configuration_window(self):
+        self.tcw.toggle_progress_window()
+
+    def setup_test_config(self, number_of_testcases, control, ten, quarter, half_hundred, hundred, two_hundred, half_million, million):
+        self.number_of_testcases = number_of_testcases
+        self.control = control
+        self.ten = ten
+        self.quarter = quarter
+        self.half_hundred = half_hundred
+        self.hundred = hundred
+        self.two_hundred = two_hundred
+        self.half_million = half_million
+        self.million = million
+        self.testcases = []
+        self.current_testcase = 0
+
+    def generate_testcases(self):
+        while len(self.testcases) < self.number_of_testcases:
+            if self.ten:
+                testcase = gen.get_ten()
+                self.testcases.append(get_nomenclatures(testcase))
+            if self.quarter:
+                testcase = gen.get_quarter()
+                self.testcases.append(get_nomenclatures(testcase))
+            if self.half_hundred:
+                testcase = gen.get_half_hundred()
+                self.testcases.append(get_nomenclatures(testcase))
+            if self.hundred:
+                testcase = gen.get_hundred()
+                self.testcases.append(get_nomenclatures(testcase))
+            if self.two_hundred:
+                testcase = gen.get_two_hundred()
+                self.testcases.append(get_nomenclatures(testcase))
+            if self.half_million:
+                testcase = gen.get_half_million()
+                self.testcases.append(get_nomenclatures(testcase))
+            if self.million:
+                testcase = gen.get_million()
+                self.testcases.append(get_nomenclatures(testcase))
+
+    def show_current_testcase(self):
+        self.clear_table()
+        self.ui.MapTable.setItem(1, 1, QTableWidgetItem(self.testcases[self.current_testcase][4]))
+        self.initialize_table()
+
+    def proceed_to_next_testcase(self):
+        self.current_testcase += 1
+        self.show_current_testcase()
+
+    def proceed_to_prev_testcase(self):
+        self.current_testcase -= 1
+        self.show_current_testcase()
+
+    def update_text(self):
+        if self.current_testcase == self.number_of_testcases-1:
+            self.ui.NextButton.setText("Завершить")
+        else:
+            self.ui.NextButton.setText("Следующий")
+        if self.current_testcase == 0:
+            self.ui.BackButton.setText("К настройкам")
+        else:
+            self.ui.BackButton.setText("Предыдущий")
+
+    def nextButtonClicked(self):
+        if self.current_testcase < self.number_of_testcases-1:
+            self.proceed_to_next_testcase()
+        self.update_text()
+
+    def backButtonClicked(self):
+        if self.current_testcase > 0:
+            self.proceed_to_prev_testcase()
+        else:
+            self.toggle_test_configuration_window()
+        self.update_text()
 
 
 class TestConfigurationWindow(QMainWindow):
@@ -22,20 +123,32 @@ class TestConfigurationWindow(QMainWindow):
         super(TestConfigurationWindow, self).__init__()
         self.ui = ui.ui_TestConfiguration.Ui_MainWindow()
         self.ui.setupUi(self)
+        self.initialize_links(MainWindow)
+        self.set_signals()
+
+    def initialize_links(self, MainWindow):
         self.mw = MainWindow
-        self.pw = TestProgress()
-        self.ui.Back.clicked.connect(self.toggle_main_window)
-        self.ui.Begin.clicked.connect(self.toggle_progress_window)
+        self.tpw = TestProgressWindow(self, MainWindow)
+        print("TCW init")
+
+    def set_signals(self):
+        self.ui.BackButton.clicked.connect(self.toggle_main_window)
+        self.ui.BeginButton.clicked.connect(self.toggle_progress_window)
 
     def toggle_main_window(self):
         self.mw.toggle_test_config_window()
 
     def toggle_progress_window(self):
-        if self.pw.isVisible():
-            self.pw.hide()
+        if self.tpw.isVisible():
+            self.tpw.hide()
             self.show()
         else:
-            self.pw.show()
+            self.tpw.setup_test_config(7, self.ui.ControlRadioButton.isChecked(), self.ui.TenCheckBox.isChecked(), self.ui.QuarterCheckBox.isChecked(), self.ui.HalfHundredCheckBox.isChecked(),
+                                        self.ui.HundredcheckBox.isChecked(), self.ui.TwoHundredCheckBox.isChecked(), self.ui.HalfMillionCheckBox.isChecked(),
+                                        self.ui.MillionCheckBox.isChecked())
+            self.tpw.generate_testcases()
+            self.tpw.show_current_testcase()
+            self.tpw.show()
             self.hide()
 
 
@@ -44,8 +157,14 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = ui.ui_mainwindow.Ui_MainWindow()
         self.ui.setupUi(self)
-        self.tcw = TestConfigurationWindow(self)
+        self.initialize_links()
+        self.set_signals()
+
+    def set_signals(self):
         self.ui.TestButton.clicked.connect(self.toggle_test_config_window)
+
+    def initialize_links(self):
+        self.tcw = TestConfigurationWindow(self)
 
     def toggle_test_config_window(self):
         if self.tcw.isVisible():
